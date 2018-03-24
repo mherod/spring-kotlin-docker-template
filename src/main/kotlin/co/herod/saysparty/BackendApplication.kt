0e937fe41f5bf1b091755b8e4c033a4c6fdf25fa
Matthew Herod <matthew.herod@myunidays.com>
2018-03-24 19:30:46 +0000

    More skeleton improvements

--- src/main/kotlin/co/herod/saysparty/BackendApplication.kt
+++ src/main/kotlin/co/herod/saysparty/BackendApplication.kt
@@ -1,5 +1,6 @@
 package co.herod.saysparty
 
+import co.herod.spring_kotlin_docker_template.BuildConfig
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
@@ -7,5 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 class BackendApplication
 
 fun main(vararg args: String) {
+    println("Project name: ${BuildConfig.PROJECT_NAME}")
+    println("Project version: ${BuildConfig.PROJECT_VERSION}")
     SpringApplication.run(BackendApplication::class.java, *args)
 }
